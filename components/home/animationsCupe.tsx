d3693099195e63d114b7c7046290e45f39be3d07
Musa Yazlık <33998073+musayazlik@users.noreply.github.com>
2022-12-12 02:05:51 +0300

    Home section update.

---
+++ components/home/animationsCupe.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const AnimationsCupe = () => {
+  return (
+    <>
+      <ul className='background-animation -z-10 bg-blue-700 dark:bg-[#1b1947] '>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </>
+  )
+}
+
+export default AnimationsCupe
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,9 @@
 import Head from 'next/head'
+import Image from 'next/image'
+import AnimationsCupe from '../components/home/animationsCupe'
 import Layout from '../components/layout'
+import { Icon } from '@iconify/react'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -12,25 +16,84 @@ export default function Home() {
       </Head>
 
       <Layout>
-        <main className='flex mb-20 '>
-          <ul className='background bg-indigo-700 dark:bg-[#1b1947] '>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-            <li></li>
-          </ul>
+        <main className='flex mb-20 min-h-[700px] pt-52 pb-24 relative '>
+          <AnimationsCupe />
+          <section className='container mx-auto max-w-3xl flex flex-col justify-center items-start rounded-3xl  '>
+            <div className='flex flex-col items-center  '>
+              <h1 className='text-5xl font-bold text-zinc-200 shadow-lg shadow-blue-800/20 bg-[conic-gradient(at_bottom,_var(--tw-gradient-stops))] from-blue-700/20 via-blue-900/20 to-zinc-900/20 backdrop-blur-sm px-14 py-6 mb-12 border-b-4 border-blue-800'>
+                SoftDeTerms
+              </h1>
+              <p className=' bg-blue-600/20 dark:bg-zinc-900/20 text-zinc-200 text-center px-10 py-8 backdrop-blur-sm font-medium text-lg leading-6 '>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+                Suscipit, et temporibus repellat nesciunt sapiente cum ducimus
+                soluta saepe similique beatae aspernatur ipsam eos numquam a
+                magni voluptas vitae natus eveniet ut provident dolore
+                voluptates minus? Temporibus quo quod cum blanditiis voluptates
+                sequi facilis dolorem excepturi vero, id quibusdam, voluptatem
+                provident dolore accusantium aliquid ea ullam, tempore saepe
+                assumenda debitis doloribus?
+              </p>
+            </div>
+
+            <div className='col-span-12 lg:col-span-4 flex w-full items-center justify-center h-full mt-16 gap-x-8  text-zinc-200'>
+              <Link href={'/'} className='hover:scale-150 duration-300 '>
+                <Icon
+                  icon='ph:facebook-logo-duotone'
+                  width={36}
+                  height={36}
+                  className='hover:drop-shadow-md'
+                />
+              </Link>
+              <Link href={'/'} className='hover:scale-150 duration-300 '>
+                <Icon
+                  icon='ph:twitter-logo-duotone'
+                  width={36}
+                  height={36}
+                  className='hover:drop-shadow-md'
+                />
+              </Link>
+              <Link
+                href={'/'}
+                className='hover:scale-150 duration-300 rounded-3xl'>
+                <Icon
+                  icon='ph:instagram-logo-duotone'
+                  width={36}
+                  height={36}
+                  className='hover:drop-shadow-md'
+                />
+              </Link>
+              <Link
+                href={'/'}
+                className='hover:scale-150 duration-300 rounded-3xl'>
+                <Icon
+                  icon='ph:linkedin-logo-duotone'
+                  width={36}
+                  height={36}
+                  className='hover:drop-shadow-md'
+                />
+              </Link>
+              <Link
+                href={'/'}
+                className='hover:scale-150 duration-300 rounded-3xl'>
+                <Icon
+                  icon='ph:github-logo-duotone'
+                  width={36}
+                  height={36}
+                  className='hover:drop-shadow-md'
+                />
+              </Link>
+              <Link
+                href={'/'}
+                className='hover:scale-150 duration-300 rounded-3xl'>
+                <Icon
+                  icon='ph:dribbble-logo-duotone'
+                  width={36}
+                  height={36}
+                  className='hover:drop-shadow-md'
+                />
+              </Link>
+            </div>
+          </section>
         </main>
       </Layout>
     </div>
