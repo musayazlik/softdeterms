88c280e82c24b35d6739930b67c8bfb9c0dcd7c7
Musa Yazlık <33998073+musayazlik@users.noreply.github.com>
2022-12-18 23:35:15 +0300

    Bugs fixed.

---
+++ components/home/homeContext/index.tsx
@@ -0,0 +1,13 @@
+const HomeContext = () => {
+  return (
+    <>
+      <div className='container mx-auto'>
+        <div className='py-20 bg-zinc-50'>
+          <h1>Home Context</h1>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default HomeContext
