ef4c9b9776a654648e96279a1f693afa394ea3cb
Musa Yazlık <33998073+musayazlik@users.noreply.github.com>
2022-12-19 12:01:29 +0300

    Dark mode bugs fix.

--- pages/contact-us/index.tsx
+++ pages/contact-us/index.tsx
@@ -12,65 +12,75 @@ const ContactUs = () => {
           <form action='' className='grid grid-cols-1 gap-6 mt-20'>
             <div className='grid grid-cols-1 sm:grid-cols-12 gap-6 px-8 md:px-20 xl:px-60 '>
               <div className='flex flex-col gap-2 col-span-10 col-start-2'>
-                <label htmlFor='name' className='font-semibold'>
+                <label
+                  htmlFor='name'
+                  className='font-semibold text-zinc-900 dark:text-zinc-100'>
                   Name
                 </label>
                 <input
                   type='text'
                   name='name'
                   id='name'
-                  className='border-2 border-gray-300 rounded-md px-4 py-2 focus:outline-double focus:outline-4 focus:outline-offset-2 focus:outline-zinc-700 duration-300 focus:border-zinc-700'
+                  className='border-2 border-gray-300 rounded-md px-4 py-2 focus:outline-double focus:outline-4 focus:outline-offset-2 focus:outline-zinc-700 dark:focus:outline-blue-600 duration-300 focus:border-zinc-700'
                 />
               </div>
             </div>
             <div className='grid grid-cols-1 sm:grid-cols-12 gap-6 px-8 md:px-20 xl:px-60 '>
               <div className='flex flex-col gap-2 col-span-10 col-start-2'>
-                <label htmlFor='surname' className='font-semibold'>
+                <label
+                  htmlFor='surname'
+                  className='font-semibold text-zinc-900 dark:text-zinc-100'>
                   Surname
                 </label>
                 <input
                   type='text'
                   name='surname'
                   id='surname'
-                  className='border-2 border-gray-300 rounded-md px-4 py-2 focus:outline-double focus:outline-4 focus:outline-offset-2 focus:outline-zinc-700 duration-300 focus:border-zinc-700'
+                  className='border-2 border-gray-300 rounded-md px-4 py-2 focus:outline-double focus:outline-4 focus:outline-offset-2 focus:outline-zinc-700 dark:focus:outline-blue-600 duration-300 focus:border-zinc-700'
                 />
               </div>
             </div>
 
             <div className='grid grid-cols-1 sm:grid-cols-12 gap-6 px-8 md:px-20 xl:px-60 '>
               <div className='flex flex-col gap-2 col-span-10 col-start-2'>
-                <label htmlFor='email' className='font-semibold'>
+                <label
+                  htmlFor='email'
+                  className='font-semibold text-zinc-900 dark:text-zinc-100'>
                   E-mail
                 </label>
                 <input
                   type='email'
                   name='email'
                   id='email'
-                  className='border-2 border-gray-300 rounded-md px-4 py-2 focus:outline-double focus:outline-4 focus:outline-offset-2 focus:outline-zinc-700 duration-300 focus:border-zinc-700'
+                  className='border-2 border-gray-300 rounded-md px-4 py-2 focus:outline-double focus:outline-4 focus:outline-offset-2 focus:outline-zinc-700 dark:focus:outline-blue-600 duration-300 focus:border-zinc-700'
                 />
               </div>
             </div>
             <div className='grid grid-cols-1 sm:grid-cols-12 gap-6 px-8 md:px-20 xl:px-60 '>
               <div className='flex flex-col gap-2  col-span-10 col-start-2'>
-                <label htmlFor='subject' className='font-semibold'>
+                <label
+                  htmlFor='subject'
+                  className='font-semibold text-zinc-900 dark:text-zinc-100'>
                   Subject
                 </label>
                 <input
                   type='text'
                   name='subject'
                   id='subject'
-                  className='border-2 border-gray-300 rounded-md px-4 py-2 focus:outline-double focus:outline-4 focus:outline-offset-2 focus:outline-zinc-700 duration-300 focus:border-zinc-700'
+                  className='border-2 border-gray-300 rounded-md px-4 py-2 focus:outline-double focus:outline-4 focus:outline-offset-2 focus:outline-zinc-700 dark:focus:outline-blue-600 duration-300 focus:border-zinc-700'
                 />
               </div>
             </div>
 
             <div className='grid grid-cols-1 sm:grid-cols-12 gap-6 px-8 md:px-20 xl:px-60   '>
               <div className='flex flex-col gap-2 col-span-10 col-start-2'>
-                <label htmlFor='message' className='font-semibold'>
+                <label
+                  htmlFor='message'
+                  className='font-semibold text-zinc-900 dark:text-zinc-100'>
                   Message
                 </label>
                 <textarea
-                  className='border-2 border-gray-300 rounded-md px-4 py-2 focus:outline-double focus:outline-4 focus:outline-offset-2 focus:outline-zinc-700 duration-300 focus:border-zinc-700'
+                  className='border-2 border-gray-300 rounded-md px-4 py-2 focus:outline-double focus:outline-4 focus:outline-offset-2 focus:outline-zinc-700 dark:focus:outline-blue-600 duration-300 focus:border-zinc-700'
                   rows={6}
                   id='message'
                 />
