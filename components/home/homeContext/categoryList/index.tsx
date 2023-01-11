0db6266ae8d89579badf7fcf2d12689ccdde7f61
Musa Yazlik <musayazlik1997@gmail.com>
2023-01-11 22:49:35 +0300

    Client data structure removed.

--- components/home/homeContext/categoryList/index.tsx
+++ components/home/homeContext/categoryList/index.tsx
@@ -1,24 +1,22 @@
 import axios from 'axios'
 import { GetServerSideProps } from 'next'
 import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import ListItem from './listItem'
 
 const CategoryList = ({ data }: any) => {
-  const [categories, setCategories] = useState<any>([])
   const [category, setCategory] = useState<string>('')
   const [activeCategory, setActiveCategory] = useState<String>('All')
+  const dispatch = useDispatch()
 
-  useEffect(() => {
-    axios.get(`${process.env.NEXT_PUBLIC_URL}/api/categories`).then((res) => {
-      setCategories(res.data)
-    })
-  }, [])
+  const categories = useSelector((state: any) => state.categories.data.data)
 
   return (
     <div className='hidden xl:flex lg:flex-col items-center w-full '>
       <ul className=' overflow-auto gap-0 text-sm font-medium text-zinc-900 dark:text-white hidden lg:flex-col lg:flex lg:space-y-6 px-4 w-full py-5 '>
-        {categories?.data?.map((category: any) => (
+        {categories?.map((category: any, index: number) => (
           <ListItem
+            key={index}
             text={category.name}
             category={category}
             setCategory={setCategory}
@@ -32,16 +30,3 @@ const CategoryList = ({ data }: any) => {
 }
 
 export default CategoryList
-
-export const getServerSideProps: GetServerSideProps<{ data: any }> = async (
-  context
-) => {
-  const res = await fetch('https://localhost:3000/api/categories')
-  const data: any = await res.json()
-
-  return {
-    props: {
-      data,
-    },
-  }
-}
