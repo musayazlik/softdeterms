9b6a66b05604db564dcb3754f25bf6a453c54a2b
Musa Yazlik <musayazlik1997@gmail.com>
2023-01-15 21:09:55 +0300

    class fix.

--- pages/sign-in/index.tsx
+++ pages/sign-in/index.tsx
@@ -54,8 +54,8 @@ const SignIn = ({ providers }: any) => {
       {/* <button onClick={() => signIn('github')}>Sign in with Github</button> */}
       <div className=''>
         <div className='flex flex-col items-center justify-center text-center relative'>
-          <div className='w-full relative min-h-[50vh] translate-y-36 z-10 flex justify-center mx-4'>
-            <div className=' flex flex-col items-center justify-center max-w-md mx-auto bg-white/95 dark:bg-zinc-900/95 shadow-zinc-800/20 shadow-lg absolute rounded-xl px-4 py-4 sm:px-10 sm:py-10 sm:outline-double sm:outline-offset-8 sm:outline-8 sm:outline-zinc-900 '>
+          <div className='w-full min-h-[50vh] z-10 flex justify-center items-center mx-4'>
+            <div className='inline-flex flex-col items-center justify-center max-w-md mx-auto bg-white/95 dark:bg-zinc-900/95 shadow-zinc-800/20 shadow-lg absolute rounded-xl px-4 py-4 sm:px-12 sm:py-20 sm:outline-double sm:outline-offset-8 sm:outline-8 sm:outline-zinc-900 top-0 bottom-0 my-auto h-fit '>
               <h1 className='text-4xl font-bold text-zinc-800 dark:text-zinc-200'>
                 Sign In
               </h1>
@@ -69,7 +69,7 @@ const SignIn = ({ providers }: any) => {
                       redirect: false,
                     })
                   }
-                  className='inline-flex items-center justify-start w-full gap-x-3 px-4 py-2.5 text-md font-semibold border-2 border-b-4 border-zinc-900 text-white bg-zinc-800 rounded-md hover:bg-zinc-900 focus:outline-none focus-visible:ring-2 duration-300 focus-visible:ring-white focus-visible:ring-opacity-75 hover:scale-105'>
+                  className='inline-flex items-center justify-start w-full gap-x-3 px-4 py-2.5 text-md font-semibold border-2 border-b-4 border-zinc-900 text-white bg-zinc-800 rounded-md hover:bg-blue-800 focus:outline-none focus-visible:ring-2 duration-300 focus-visible:ring-white focus-visible:ring-opacity-75 hover:scale-105'>
                   <Icon icon='ph:github-logo-duotone' width={30} />
                   Sign in with Github
                 </button>
@@ -80,23 +80,23 @@ const SignIn = ({ providers }: any) => {
                     })
                   }
                   className='inline-flex items-center justify-start w-full gap-x-3.5 px-4 py-2.5 text-md border-2 border-b-4 border-zinc-400 font-semibold text-zinc-600 bg-white rounded-md duration-300 hover:bg-zinc-400 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 hover:scale-105'>
-                  <Icon icon='flat-color-icons:google' width={28} />
-                  Sign in with google
+                  <Icon icon='eva:google-fill' width={28} />
+                  Sign in with Google
                 </button>
-                <button
+                {/* <button
                   onClick={() =>
                     signIn('facebook', {
                       redirect: false,
                     })
                   }
-                  className='inline-flex items-center justify-start w-full gap-x-3 px-4 py-2.5 text-md border-2 border-b-4 border-blue-800 font-semibold text-white bg-blue-600 rounded-md duration-300 hover:bg-blue-800 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 hover:scale-105'>
+                  className='inline-flex items-center justify-start w-full gap-x-3 px-4 py-2.5 text-md border-2 border-b-4 border-blue-700 font-semibold text-white bg-blue-500 rounded-md duration-300 hover:bg-blue-700 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 hover:scale-105'>
                   <Icon
                     icon='ph:facebook-logo-duotone'
                     className='text-white'
-                    width={28}
+                    width={26}
                   />
                   Sign in with Facebook
-                </button>
+                </button> */}
               </div>
             </div>
           </div>
