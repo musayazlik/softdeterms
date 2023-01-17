1dfdfea837d945a1fdca408463c7a176ec031b65
Musa Yazlik <musayazlik1997@gmail.com>
2023-01-17 23:25:09 +0300

    Post field.

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -9,34 +9,34 @@ import { useEffect, useState } from 'react'
 import axios from 'axios'
 import Head from 'next/head'
 import { useSelector } from 'react-redux'
+import Loading from '@components/loading'
 
-const PostCreate = () => {
+const PostDetail = () => {
   const router = useRouter()
   const [post, setPost] = useState<any>([] as any)
+  const [loading, setLoading] = useState<boolean>(true)
 
   useEffect(() => {
+    router.prefetch('/')
     axios({
       method: 'GET',
       url: `${process.env.NEXT_PUBLIC_URL}/api/posts/${router.query.id}`,
     })
       .then((res) => {
-        setPost(res.data)
+        setPost(res.data.posts)
+        setLoading(false)
       })
       .catch((err) => {
         console.log(err)
       })
   }, [])
 
-  const datalar = useSelector((state: any) => state.posts.data)
-  const stateData = datalar?.filter((item: any) => item._id === router.query.id)
-  console.log(stateData)
-
   return (
     <>
       <Head>
-        <title>{stateData[0]?.title}</title>
-        <meta name='description' content={stateData[0]?.description} />
-        <meta name='author' content={stateData[0]?.author} />
+        <title>{post?.title}</title>
+        <meta name='description' content={post?.description} />
+        <meta name='author' content={post?.author} />
       </Head>
       <Layout>
         <div className='container mx-auto mt-40 mb-20'>
@@ -60,14 +60,14 @@ const PostCreate = () => {
                 <div className='content-informations bg-zinc-100 dark:bg-zinc-900 border-b-4 border-zinc-600 dark:border-blue-600 py-4 px-8 flex space-x-10 rounded-t-2xl'>
                   <div className=' flex flex-col items-center space-y-2 justify-center '>
                     <Image
-                      src={stateData[0]?.userId?.image}
+                      src={post?.userId?.image}
                       className='rounded-full outline-offset-0 	outline-zinc-700 dark:outline-zinc-400 dark:outline-offset-2 outline-2 outline-dashed'
                       width={80}
                       height={80}
                       alt='Profile Photo'
                     />
                     <div className='font-semibold text-zinc-800 dark:text-zinc-400 whitespace-nowrap'>
-                      {stateData[0]?.author}
+                      {post?.author}
                     </div>
                     <div className='social'>
                       <div className='userSocial flex gap-2'>
@@ -103,24 +103,26 @@ const PostCreate = () => {
                   </div>
                   <div className='flex flex-col space-y-2 py-2'>
                     <h1 className='text-2xl font-bold tracking-tight text-zinc-800 dark:text-zinc-400'>
-                      {stateData[0]?.title}
+                      {post?.title}
                     </h1>
 
                     <p className='text-zinc-800 dark:text-zinc-400 tracking-tight '>
-                      {stateData[0]?.description}
+                      {post?.description}
                     </p>
                   </div>
                 </div>
                 <div className='p-6 text-zinc-800 dark:text-zinc-400'>
-                  {Parse(decode(post.posts?.content))}
+                  {Parse(decode(post?.content))}
                 </div>
               </div>
             </div>
           </div>
         </div>
       </Layout>
+
+      {loading && <Loading />}
     </>
   )
 }
 
-export default PostCreate
+export default PostDetail
