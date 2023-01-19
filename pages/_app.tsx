399855157f39a4a552e8955d8e331fa552f254cb
Musa Yazlık <33998073+musayazlik@users.noreply.github.com>
2023-01-19 14:50:07 +0300

    Store provider update.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,17 +6,21 @@ import type { AppProps } from 'next/app'
 import { SessionProvider } from 'next-auth/react'
 import MaintenanceMode from '../components/maintenanceMode'
 import { wrapper } from 'store'
+import { Provider } from 'react-redux'
 
 const App = ({ Component, pageProps: { session, ...pageProps } }: AppProps) => {
+  const { store } = wrapper.useWrappedStore(pageProps)
   if (process.env.NEXT_PUBLIC_MAINTENANCE_MODE === 'true') {
     return <MaintenanceMode />
   } else {
     return (
-      <SessionProvider session={session}>
-        <Component {...pageProps} />
-      </SessionProvider>
+      <Provider store={store}>
+        <SessionProvider session={session}>
+          <Component {...pageProps} />
+        </SessionProvider>
+      </Provider>
     )
   }
 }
 
-export default wrapper.withRedux(App)
+export default App
